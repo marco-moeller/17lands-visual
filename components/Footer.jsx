2791dea092b542efb7ace6e67196f68289e65b62
Marco Moeller <maaakooo@gmx.de>
2023-11-14 16:40:53 -0800

    footer
    added a footer with fair use stuff to not get sued

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Portions of this site are unofficial fan content permitted under the
+        Wizards of the Coast Fan Content Policy. The literal and graphical
+        information presented on this site about Magic: The Gathering, including
+        card images, mana symbols, and Oracle text, site background is copyright
+        Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. This site is not
+        produced by or endorsed by Wizards of the Coast.
+      </p>{" "}
+      <p>Everythibg else you see - © Marco Moeller - 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
