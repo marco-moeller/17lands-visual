42d26d0b908a2faba52ec4e323e04e3686d9ab9b
Marco Moeller <maaakooo@gmx.de>
2024-02-19 19:34:15 -0800

    footer update
    added mentioning of 17Lands in the footer - oops

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,12 +7,20 @@ const Footer = () => {
         Portions of this site are unofficial fan content permitted under the
         Wizards of the Coast Fan Content Policy. The literal and graphical
         information presented on this site about Magic: The Gathering, including
-        card images, mana symbols, and Oracle text, site background is copyright
-        Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. This site is not
-        produced by or endorsed by Wizards of the Coast.
-      </p>{" "}
+        card images, mana symbols, and Oracle text is copyright Wizards of the
+        Coast, LLC, a subsidiary of Hasbro, Inc. This site is not produced by or
+        endorsed by Wizards of the Coast.
+      </p>
       <p>
-        Everythibg else you see - © Marco Moeller - {new Date().getFullYear()}
+        The card win rates above are retrieved from{" "}
+        <a href="https://www.17lands.com" target="_blank">
+          17Lands - www.17lands.com
+        </a>
+        . Download their tracker to have your drafts recorded and added to their
+        dataset. This site is not produced by or endorsed by 17Lands.
+      </p>
+      <p>
+        Everything else you see - © Marco Moeller - {new Date().getFullYear()}
       </p>
     </footer>
   );
