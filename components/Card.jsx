ffceb1d49fc7ca43c078b2b7e01c35b47b2737f1
Marco Moeller <maaakooo@gmx.de>
2024-06-12 12:20:31 -0700

    flip double faced cards
    add button to flip double faced cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,17 @@
 import Modal from "./Modal";
 import useModal from "@/app/hooks/useModal";
+import { useState } from "react";
+import { MdFlipCameraAndroid } from "react-icons/md";
 
 function Card({ card }) {
   const { isShowing, toggle } = useModal();
+  const [isShowFront, setShowFront] = useState(true);
+
+  const isDoubleFace = card.url_back !== "";
+
+  const handleFlip = () => {
+    setShowFront((prevState) => !prevState);
+  };
 
   return (
     <>
@@ -11,11 +20,43 @@ function Card({ card }) {
           <p className="label">GIHWR</p>
           {(card.ever_drawn_win_rate * 100).toFixed(2)}%
         </h2>
-        <img className="card-img" src={card.url} alt="card" />
+        <img
+          className="card-img"
+          src={isShowFront ? card.url : card.url_back}
+          alt="card"
+        />
+        {isDoubleFace && (
+          <button
+            className="flip--btn"
+            onClick={(e) => {
+              handleFlip();
+              e.stopPropagation();
+            }}
+          >
+            {" "}
+            <MdFlipCameraAndroid />
+          </button>
+        )}
       </div>
       {isShowing && (
         <Modal toggle={toggle}>
-          <img className="card-img" src={card.url} alt="card" />
+          <img
+            className="card-img"
+            src={isShowFront ? card.url : card.url_back}
+            alt="card"
+          />
+          {isDoubleFace && (
+            <button
+              className="flip--btn"
+              onClick={(e) => {
+                handleFlip();
+                e.stopPropagation();
+              }}
+            >
+              {" "}
+              <MdFlipCameraAndroid />
+            </button>
+          )}
         </Modal>
       )}
     </>
